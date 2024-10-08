01b84b008b44e91ad5f65b3c1dbb2e63cafbd76b
Beexoul <game.beexoul@gmail.com>
2024-10-08 18:23:27 +0545

    new updates

--- src/Components/chatbox/Chatbox.jsx
+++ src/Components/chatbox/Chatbox.jsx
@@ -11,6 +11,36 @@ const Chatbox = () => {
         <p>Safal Neupane <img src={assets.green_dot} className='dot' alt="" /></p>
         <img src={assets.help_icon} alt="" />
       </div>  
+
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg"> My Name is Safal Neupane, I am Gay </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p className="time">2:30 PM</p>
+          </div>
+        </div>
+        <div className="s-msg">
+          <img className='msg-img' src={assets.pic1} alt="" />
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p className="time">2:30 PM</p>
+          </div>
+        </div>
+        <div className="r-msg">
+          <p className="msg"> My Name is Safal Neupane, I am Gay </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p className="time">2:30 PM</p>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
       <div className="chat-input">
         <input type="text" placeholder='send a message' />
         <input type="file" id='image' accept='image/png, image/jpeg' hidden />

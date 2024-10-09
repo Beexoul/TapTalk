4efb217e47f2485f5242553c16b217e73eda545d
Beexoul <game.beexoul@gmail.com>
2024-10-09 11:14:26 +0545

    UI Done yepppiiiiiiiiiiiiiiiiiiiiiiiii

--- src/Pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/Pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
+
+
+  const [image,setImage] = useState(false) 
+
+
+
   return (
-    <div>
+    <div className='profile'>
+        <div className="profile-container">
+        <form>
+        <h3>Profile Details</h3>
+        <label htmlFor="avatar">
+         < input onChange={(e)=>setImage(e.target.files[0])}  type="file" id='avatar' accept='.png, .jpg, .jpeg, .gif' hidden />
+         <img src={image? URL.createObjectURL(image): assets.avatar_icon} alt="" />
+        upload Profile image
+        </label>
+        <input type="text" placeholder='Your Name' required/>
+        <textarea placeholder='BIO' required></textarea>
+        <button type='submit'>Save</button>
+        </form>
+        <img src={image? URL.createObjectURL(image):assets.logo_icon} className='profile-pic' />
+      </div>
     </div>
   )
 }

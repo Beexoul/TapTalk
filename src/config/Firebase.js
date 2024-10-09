1b681edc91864bfac57cb103f2098f1feb4eac57
Beexoul <game.beexoul@gmail.com>
2024-10-09 17:52:04 +0545

    Firebase update

---
+++ src/config/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrppN8Mv_x1HomvmI9Alwiwo7Wz5R3fIc",
+  authDomain: "taptalk-3081d.firebaseapp.com",
+  projectId: "taptalk-3081d",
+  storageBucket: "taptalk-3081d.appspot.com",
+  messagingSenderId: "593241792435",
+  appId: "1:593241792435:web:7139e59e8636c5fafc35c6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
